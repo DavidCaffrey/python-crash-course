6436b85b6ecef20145c68a63bf5a9dcfc087207e
David Caffrey <dave.caffrey71@gmail.com>
2023-07-20 13:38:38 +0100

    update 20/07

---
+++ 06-CH/users.py
@@ -0,0 +1,16 @@
+user_01 = {"username": "Dacffee=d", "first_name": "David", "last_name": "Caffrey"}
+
+for key, value in user_01.items():
+    print(f"\nkey: {key}")
+    print(f"value: {value}")
+
+for k in user_01.keys():
+    print(f"\nkey: {k}")
+
+for v in user_01.values():
+    print(f"\nvalue: {v}")
+# set looks like a dictionary but does not have key value pairs
+user_02_set = {"python", "java", "python", "go"}
+print(user_02_set)
+# pdf 142
+# book 105
